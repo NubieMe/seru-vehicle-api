1682c94d56023533dc22d8e4eb95b41e01760d85
NubieMe <fahmi.mellow@gmail.com>
2024-07-03 14:22:18 +0700

    update: add page model

---
+++ src/models/page.ts
@@ -0,0 +1,33 @@
+import { brandResponse } from "./brand";
+
+export type pageRequest = {
+    page: number;
+};
+
+export type metadata = {
+    total: number;
+    limit: number;
+    offset: number;
+    current_page: number;
+    total_pages: number;
+};
+
+export type data = brandResponse[];
+
+export type pageResponse = {
+    data: data;
+    metadata: metadata;
+};
+
+export function toPageResponse(data: data, total: number, skip: number): pageResponse {
+    return {
+        data,
+        metadata: {
+            total,
+            limit: 5,
+            offset: skip,
+            current_page: skip / 5,
+            total_pages: total % 5 === 0 ? total / 5 : total / 5 + 1,
+        },
+    };
+}
