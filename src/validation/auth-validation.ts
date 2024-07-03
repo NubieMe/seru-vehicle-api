4a259d6a78dc268c21289067eab1f8758ae2e05b
NubieMe <fahmi.mellow@gmail.com>
2024-07-03 10:58:50 +0700

    fix: validation schema fixed

--- src/validation/auth-validation.ts
+++ src/validation/auth-validation.ts
@@ -1,13 +1,13 @@
 import j, { Schema } from "joi";
 
 export class authSchema {
-    private readonly register: Schema = j.object({
+    static readonly register: Schema = j.object({
         name: j.string().min(1).max(100).required(),
         password: j.string().min(1).max(100).required(),
         is_admin: j.boolean().default(false),
     });
 
-    private readonly login: Schema = j.object({
+    static readonly login: Schema = j.object({
         name: j.string().min(1).max(100).required(),
         password: j.string().min(1).max(100).required(),
     });
--- src/validation/create-validation.ts
+++ src/validation/create-validation.ts
@@ -1,25 +1,25 @@
 import j, { Schema } from "joi";
 
 export class createSchema {
-    private readonly createBrand: Schema = j.object({
+    static readonly createBrand: Schema = j.object({
         name: j.string().min(1).max(20).required(),
     });
 
-    private readonly createType: Schema = j.object({
+    static readonly createType: Schema = j.object({
         name: j.string().min(1).max(20).required(),
         brand_id: j.number().min(1).required(),
     });
 
-    private readonly createModel: Schema = j.object({
+    static readonly createModel: Schema = j.object({
         name: j.string().min(1).max(20).required(),
         type_id: j.number().min(1).required(),
     });
 
-    private readonly createYear: Schema = j.object({
+    static readonly createYear: Schema = j.object({
         year: j.string().min(1).max(4).required(),
     });
 
-    private readonly pricelist: Schema = j.object({
+    static readonly pricelist: Schema = j.object({
         code: j.string().min(1).max(20).required(),
         price: j.number().min(1).required(),
         year_id: j.number().min(1).required(),
