fbb61bbc6e4c172f939d064db7b7ca4442d6faab
NubieMe <fahmi.mellow@gmail.com>
2024-07-03 15:41:17 +0700

    update: insert auth-middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { AuthController } from "../controllers/auth-controller";
 import { BrandController } from "../controllers/brand-controller";
+import { auth } from "../middlewares/auth";
 
 export const routes = express.Router();
 
@@ -9,8 +10,8 @@ routes.post("/register", AuthController.register);
 routes.post("/login", AuthController.login);
 
 // brand API
-routes.post("/brand", BrandController.create);
-routes.get("/brand", BrandController.getAll);
-routes.get("/brand/:id", BrandController.getOne);
-routes.put("/brand/:id", BrandController.update);
-routes.delete("/brand/:id", BrandController.delete);
+routes.post("/brand", auth, BrandController.create);
+routes.get("/brand", auth, BrandController.getAll);
+routes.get("/brand/:id", auth, BrandController.getOne);
+routes.put("/brand/:id", auth, BrandController.update);
+routes.delete("/brand/:id", auth, BrandController.delete);
