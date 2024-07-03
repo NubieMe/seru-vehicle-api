d8230005b9c18386fdfa0bd388935477123440d6
NubieMe <fahmi.mellow@gmail.com>
2024-07-03 11:02:21 +0700

    update: register middleware & routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { routes } from "./routes";
+import { errorHandler } from "./middlewares/error-handler";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use("/api", routes);
+app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
